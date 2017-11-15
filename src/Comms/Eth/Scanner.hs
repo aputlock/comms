e86984c2144bc207d2819dc20ef8a53414aae309
Andrew Putlock <andrewputlock@gmail.com>
2017-11-14 22:17:05 -0500

    Update Web3 Library Version

--- src/Comms/Eth/Scanner.hs
+++ src/Comms/Eth/Scanner.hs
@@ -3,7 +3,7 @@ module Comms.Eth.Scanner where
 import Comms.Eth.Tools
 import Control.Monad.IO.Class
 import Network.Ethereum.Web3
-import Network.Ethereum.Web3.Api
+import Network.Ethereum.Web3.Eth
 import Network.Ethereum.Web3.Types
 import Network.Ethereum.Web3.TH
 
