ee3acfd8770c45b5c571b2413d6bbc15f17c974b
Brian Oluwo <broluwo@umd.edu>
2017-11-15 23:58:33 -0500

    Implement the first iteration of CLI arg parsing
    Essentially this commit sets the foundation for being able to parse a
    config file that contains the information needed to run the collection
    of servers that make up this project.
    
    It is expected that more fields will need to be added but the included
    fields are sufficient for a first attempt.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,47 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings  #-}
+
+-- ^ For info about DeriveDataTypable, see http://chrisdone.com/posts/data-typeable
+-- ^ OverloadedString allows String to be treated as either the type String or Data.Text
 module Main where
 
-import Comms.Eth.Scanner
-import Comms.Eth.Sender
+import           Comms.Eth.Scanner
+import           Comms.Eth.Sender
+import           Control.Monad
+import           Data.Aeson             ((.:), (.=))
+import qualified Data.Aeson             as JSON
+import           System.Console.CmdArgs
+import           System.Environment
+
+-- TODO(broluwo): Consider writing a generator function for creating the config file.
+-- TODO(broluwo): Document the format of the config file. It should map 1-to-1 with ServerConfig.
+data Options = Options
+       -- | Defines the location of the config file.
+  { config :: FilePath
+  -- | Enables debug printing.
+  , debug  :: Bool
+  } deriving (Data, Typeable, Show, Eq)
+
+data ServerConfig = ServerConfig
+  { smtpPort :: Int
+  , imapPort :: Int
+  , ethNode  :: String
+  } deriving (Show)
+
+options =
+  cmdArgsMode $
+  Options
+  { debug = def &= help "Print debugging info about the server"
+  , config = def &= args &= typFile
+  } &=
+  program "Comms" &=
+  details
+    [ "Expects the location of the configuration file as an argument to the program."
+    ] &=
+  help "Decentralized email server." &=
+  helpArg [explicit, name "h", name "help"] &=
+  summary "Comms v0.0.0, (c) Brian Oluwo, Andrew Putlock"
 
 main :: IO ()
-main = putStrLn "test"
+main = print =<< cmdArgsRun options
+
