880eb459b1b47c3d2e2d05ca07f37e8c9f8cd4bf
Andrew Putlock <andrewputlock@gmail.com>
2017-12-06 17:38:33 -0500

    Config creation utility
    - users are now interactively prompted to create a new config if they don't already have one

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,17 @@ module Main
   ) where
 
 import           Comms.Types
-import           Comms.Common.Util
+import           Comms.Common.Util             (configFolder, configFile, getDefaultConfig, writeConfig, runServer, fromRight)
 import qualified Comms.POP3.Server             as POP3
 import qualified Comms.SMTP.Server             as SMTP
 import qualified Comms.Eth.AddressBook         as AB (importContact)
 import qualified Comms.Eth.Sender              as S (sendContactCard)
+import           Control.Monad          (when)
 import           System.Console.CmdArgs
 import           System.Environment
 import           System.Directory
 import qualified Data.Text                     as T
+import           Network.Ethereum.Web3.Address
 -- TODO(broluwo): Consider writing a generator function for creating the config file.
 run =
   RunServerOptions
@@ -39,11 +41,11 @@ commandMode = cmdArgsMode $ modes [run &= auto, importContact, publishContact] &
 main :: IO ()
 main = do
   args <- getArgs
-  createDirectoryIfMissing True =<< configFolder
   case args of
     [] -> putStrLn "Please use the -h flag on the executable to see the usage text."
     _ -> do
       opts <- cmdArgsRun $ commandMode
+      setupFolder
       case opts of
         RunServerOptions debug -> do
           config <- getDefaultConfig
@@ -56,3 +58,25 @@ main = do
           case maybeHash of
             Left err -> putStrLn $ show err
             Right hash -> putStrLn $ "Posted contact card: " ++ T.unpack hash
+                         
+setupFolder :: IO ()
+setupFolder = do
+  createDirectoryIfMissing True =<< configFolder
+  configExists <- doesFileExist =<< configFile
+  when (not configExists) (do
+                            putStrLn "Ethereum wallet address: "
+                            walletStr <- getLine
+                            let wallet = fromRight $ fromText $ T.pack walletStr :: Address
+                            putStrLn "SMTP port: "
+                            smtpPortStr <- getLine
+                            let smtpPort = read smtpPortStr :: Int
+                            putStrLn "POP3 port: "
+                            pop3PortStr <- getLine
+                            let pop3Port = read pop3PortStr :: Int
+                            putStrLn "Ethereum node URL (blank for http://localhost:8545):"
+                            nodeURLStr <- getLine
+                            let nodeURL = if nodeURLStr == "" then Nothing else Just nodeURLStr
+                            let config = Config wallet smtpPort pop3Port nodeURL
+                            file <- configFile
+                            writeConfig file config
+                          )
